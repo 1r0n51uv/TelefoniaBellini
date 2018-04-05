606954f7edc2cf841fabdab053150bb308fedcbe
Unknown <mirkoali24@gmail.com>
2018-04-05 11:20:16 +0200

    Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,30 @@
+<footer class="footer" data-background-color="black">
+    <div class="container">
+        <nav>
+            <ul>
+                <li>
+                    <a href="#pablo" class="btn btn-neutral btn-twitter btn-icon btn-round" rel="tooltip" title="Follow us">
+                        <i class="fab fa-twitter" style="font-size:120%; margin-top:25%"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#pablo" class="btn btn-neutral btn-icon btn-round" rel="tooltip" title="Like us">
+                        <i class="fab fa-facebook-square" style= "color: #3b5998; font-size:120%; margin-top:25%"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#pablo" class="btn btn-neutral btn-icon btn-round" rel="tooltip" title="Follow us">
+                        <i class="fab fa-instagram" style= "color: #e95950; font-size:120%; margin-top:25%"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright">
+
+            <br>
+            <a>&copy; Designed</a>
+            <a target="_blank">& Coded by</a>
+            <a target="_blank">SP CORPORATION</a>
+        </div>
+    </div>
+</footer>
