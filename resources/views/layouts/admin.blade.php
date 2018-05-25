633c13132a4b8ae210b9f00403e62270720cdeb6
Unknown <mirkoali24@gmail.com>
2018-05-25 14:12:57 +0200

    Admin Template

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,121 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Welcome To | Bootstrap Based Admin Template - Material Design</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core Css -->
+    <link href="../adminassets/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href="../adminassets/plugins/node-waves/waves.css" rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href="../adminassets/plugins/animate-css/animate.css" rel="stylesheet" />
+
+    <!-- Morris Chart Css-->
+    <link href="../adminassets/plugins/morrisjs/morris.css" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="../adminassets/css/style.css" rel="stylesheet">
+
+    <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+    <link href="../adminassets/css/themes/all-themes.css" rel="stylesheet" />
+</head>
+
+<body class="theme-red">
+
+@yield('content')
+
+<!-- Jquery Core Js -->
+<script src="../adminassets/plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core Js -->
+<script src="../adminassets/plugins/bootstrap/js/bootstrap.js"></script>
+
+<!-- Select Plugin Js -->
+<script src="../adminassets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+<!-- Slimscroll Plugin Js -->
+<script src="../adminassets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+<!-- Waves Effect Plugin Js -->
+<script src="../adminassets/plugins/node-waves/waves.js"></script>
+
+<!-- Jquery CountTo Plugin Js -->
+<script src="../adminassets/plugins/jquery-countto/jquery.countTo.js"></script>
+
+<!-- Morris Plugin Js -->
+<script src="../adminassets/plugins/raphael/raphael.min.js"></script>
+<script src="../adminassets/plugins/morrisjs/morris.js"></script>
+
+<!-- ChartJs -->
+<script src=../adminassets/"plugins/chartjs/Chart.bundle.js"></script>
+
+<!-- Flot Charts Plugin Js -->
+<script src="../adminassets/plugins/flot-charts/jquery.flot.js"></script>
+<script src="../adminassets/plugins/flot-charts/jquery.flot.resize.js"></script>
+<script src="../adminassets/plugins/flot-charts/jquery.flot.pie.js"></script>
+<script src="../adminassets/plugins/flot-charts/jquery.flot.categories.js"></script>
+<script src="../adminassets/plugins/flot-charts/jquery.flot.time.js"></script>
+
+<!-- Sparkline Chart Plugin Js -->
+<script src="../adminassets/plugins/jquery-sparkline/jquery.sparkline.js"></script>
+
+<!-- Custom Js -->
+<script src="../adminassets/js/admin.js"></script>
+<script src="../adminassets/js/pages/index.js"></script>
+
+<!-- Demo Js -->
+<script src="../adminassets/js/demo.js"></script>
+
+
+<!-- Jquery Core Js -->
+<script src="../adminassets/plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core Js -->
+<script src="../adminassets/plugins/bootstrap/js/bootstrap.js"></script>
+
+<!-- Select Plugin Js -->
+<script src="../adminassets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+<!-- Slimscroll Plugin Js -->
+<script src="../adminassets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+<!-- Waves Effect Plugin Js -->
+<script src="../adminassets/plugins/node-waves/waves.js"></script>
+
+<!-- Jquery CountTo Plugin Js -->
+<script src="../adminassets/plugins/jquery-countto/jquery.countTo.js"></script>
+
+<!-- Morris Plugin Js -->
+<script src="../adminassets/plugins/raphael/raphael.min.js"></script>
+<script src="../adminassets/plugins/morrisjs/morris.js"></script>
+
+<!-- ChartJs -->
+<script src="../adminassets/plugins/chartjs/Chart.bundle.js"></script>
+
+<!-- Flot Charts Plugin Js -->
+<script src="../adminassets/plugins/flot-charts/jquery.flot.js"></script>
+<script src="../adminassets/plugins/flot-charts/jquery.flot.resize.js"></script>
+<script src="../adminassets/plugins/flot-charts/jquery.flot.pie.js"></script>
+<script src="../adminassets/plugins/flot-charts/jquery.flot.categories.js"></script>
+<script src="../adminassets/plugins/flot-charts/jquery.flot.time.js"></script>
+
+<!-- Sparkline Chart Plugin Js -->
+<script src="../adminassets/plugins/jquery-sparkline/jquery.sparkline.js"></script>
+
+<!-- Custom Js -->
+<script src="../adminassets/js/admin.js"></script>
+<script src="../adminassets/js/pages/index.js"></script>
+
+<!-- Demo Js -->
+<script src="../adminassets/js/demo.js"></script>
+
+</body>
