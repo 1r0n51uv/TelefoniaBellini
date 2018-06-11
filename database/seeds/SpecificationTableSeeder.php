62daf464150b74886045d7a1d70f56844191eb1d
Unknown <mirkoali24@gmail.com>
2018-06-11 14:02:03 +0200

    specification seed

---
+++ database/seeds/SpecificationTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SpecificationTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('specification')->insert(array(
+
+            array('id'=>'1','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'2','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'3','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'4','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'5','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'6','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'7','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'8','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'9','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'10','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'11','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'12','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'13','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'14','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+            array('id'=>'15','pic1'=>'P10','pic2'=>'P10','pic3'=>'P10','marca'=>'huawei','model'=>'P10','peso'=>'1.92','anno'=>'2017','memory'=>'256','sistem'=>'andorid','processor'=>'2.0','screen'=>'P10','display'=>'P10','resolution'=>'P10','camera'=>'P10','media'=>'P10','autofocus'=>'P10','bluetooth'=>'P10',),
+
+
+
+
+        ));
+    }
+}
