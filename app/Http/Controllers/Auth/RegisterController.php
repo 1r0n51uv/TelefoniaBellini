1e9b163280193562f8f38d2074a929e59c67badb
Unknown <s.ammirati95@gmail.com>
2018-07-02 11:39:36 +0200

    Varie cose funzionali

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-
+use Gravatar;
 class RegisterController extends Controller
 {
     /*
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'surname' => $data['surname'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-
+            'fb_pic' => '../assets/img/stock.png',
         ]);
     }
 }
