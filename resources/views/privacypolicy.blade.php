06514b7e7b3c2e36bd16127ed9a63447435e9b94
Unknown <mirkoali24@gmail.com>
2018-07-24 14:02:37 +0200

    Revert "Revert "privacy policy italiano""
    This reverts commit 7891e8c82ee9736708b03a6d146eccd8abe0d6c7.

--- resources/views/privacypolicy.blade.php
+++ resources/views/privacypolicy.blade.php
@@ -1,108 +1,113 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="section" style="margin-left: 1%; margin-top: 1%;">
-    <h1>Privacy Policy</h1>
-
-    <p>Telefonia Belliini ("us", "we", or "our") operates the https://telefoniabellini.it/ website (the "Service").</p>
-
-    <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data. This Privacy Policy  for Telefonia Belliini is powered by <a href="https://www.freeprivacypolicy.com/free-privacy-policy-generator.php">FreePrivacyPolicy.com</a>.</p>
-
-    <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from https://telefoniabellini.it/</p>
 
 
-    <h2>Information Collection And Use</h2>
+<div class="section" style="margin-top: 1%; padding-left: 1%; background-color:lightgrey ">
+    <h1> Informativa sulla privacy </h1>
 
-    <p>We collect several different types of information for various purposes to provide and improve our Service to you.</p>
+    <p> Telefonia Bellini gestisce il sito https://telefoniabellini.it/ ("servizio"). </p>
 
-    <h3>Types of Data Collected</h3>
+    <p> Questa pagina ti informa delle nostre politiche riguardanti la raccolta, l'uso e la divulgazione dei dati personali quando si usufruisce del servizio e le scelte che hai associato a quei dati.</p>
 
-    <h4>Personal Data</h4>
+    <p> Utilizziamo i tuoi dati per fornire e migliorare il servizio. Interfacciandosi con l'utente accetta la raccolta e l'utilizzo delle informazioni in conformità con questa politica.</p>
 
-    <p>While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you ("Personal Data"). Personally identifiable information may include, but is not limited to:</p>
 
-    <ul>
-        <li>Email address</li><li>First name and last name</li><li>Address, State, Province, ZIP/Postal code, City</li><li>Cookies and Usage Data</li>
+    <h2> Raccolta e utilizzo delle informazioni </h2>
+
+    <p> Raccogliamo diversi tipi di informazioni per vari scopi al fin di fornire e migliorare il nostro website per te. </p>
+
+    <h3> Tipi di dati raccolti </h3>
+
+    <h4> Dati personali </h4>
+
+    <p> Durante l'utilizzo del nostro Servizio, potremmo chiederti di fornirci alcune informazioni di identificazione personale che possono essere utilizzate per contattarti o identificarti ("Dati personali"). Le informazioni di identificazione personale possono includere, ma non sono limitate a: </p>
+
+    <Ul>
+        <li> Indirizzo email </li> <li> Nome e cognome </li> <li> Indirizzo, stato, provincia, codice di avviamento postale, città </li> <li> cookie e dati di utilizzo </li>
+    </Ul>
+
+    <h4> Dati di utilizzo </h4>
+
+    <p> Potremmo anche raccogliere informazioni su come accedere e utilizzare i "Dati di utilizzo". Questi dati di utilizzo possono includere informazioni quali l'indirizzo del protocollo Internet del computer (ad es. Indirizzo IP), il tipo di browser, la versione del browser, le pagine del nostro servizio che si visita.</p>
+
+    <h4> Dati di tracciamento e cookie </h4>
+    <p> Utilizziamo cookie e tecnologie di tracciamento simili per tracciare l'attività del sito e conservare determinate informazioni. </p>
+    <p> I cookie sono file con una piccola quantità di dati che possono includere un identificatore univoco anonimo. I cookie vengono inviati al tuo browser da un sito web e memorizzati sul tuo dispositivo. Le tecnologie di monitoraggio utilizzate sono anche beacon, tag e script per raccogliere e tenere traccia delle informazioni e per migliorare e analizzare il nostro servizio. </p>
+    <p> Puoi chiedere al tuo browser di rifiutare tutti i cookie o di indicare quando viene inviato un cookie. Tuttavia, se non accetti i cookie, potresti non essere in grado di utilizzare alcune parti del sito.</p>
+    <p> Esempi di cookie che utilizziamo: </p>
+    <Ul>
+            <li> <strong> Cookie di sessione. </strong> Utilizziamo i cookie di sessione per gestire il nostro servizio. </li>
+            <li> <strong> Cookie di preferenze. </strong> Utilizziamo i cookie di preferenza per ricordare le tue preferenze e varie impostazioni. </li>
+            <li> <strong> Cookie di sicurezza. </strong> Utilizziamo i cookie di sicurezza per motivi di sicurezza. </li>
+    </Ul>
+
+    <h2> Uso dei dati </h2>
+        
+    <p> Telefonia Belliini utilizza i dati raccolti per vari scopi: </p>
+    <Ul>
+            <li> Fornire e mantenere il servizio </li>
+            <li> Per notificare le modifiche al nostro servizio </li>
+            <li> Per consentirti di partecipare alle funzionalità interattive del servizio quando scegli di farlo </li>
+            <li> Fornire assistenza e supporto ai clienti </li>
+            <li> Fornire analisi o informazioni preziose per migliorare il servizio </li>
+            <li> Per monitorare l'utilizzo del servizio </li>
+            <li> Per rilevare, prevenire e risolvere i problemi tecnici </li>
+    </Ul>
+
+    <h2> Trasferimento di dati </h2>
+    <p> Le tue informazioni, compresi i Dati personali, possono essere trasferite a e mantenute su computer situati al di fuori del tuo stato, provincia, nazione o altra giurisdizione governativa in cui le leggi sulla protezione dei dati possono essere diverse da quelle della tua giurisdizione. </p>
+    <p> Se ti trovi al di fuori dell'Italia e scegli di fornirci informazioni, ti preghiamo di notare che trasferiamo i dati, inclusi i Dati personali, in Italia e li elaboriamo lì. </p>
+    <p> Il tuo consenso a questa Informativa sulla privacy seguito dall'invio di tali informazioni rappresenta il tuo consenso a tale trasferimento. </p>
+    <p> Telefonia Bellini adotterà tutte le misure ragionevolmente necessarie per garantire che i tuoi dati siano trattati in modo sicuro e in conformità con la presente Informativa sulla privacy e nessun trasferimento dei tuoi Dati personali sarà effettuato presso un'organizzazione o un paese a meno che non vi siano controlli adeguati in atto la sicurezza dei tuoi dati e altre informazioni personali. </p>
+
+    <h2> Divulgazione dei dati </h2>
+
+    <h3> Requisiti legali </h3>
+    <p> Telefonia Bellini potrebbe divulgare i vostri dati personali in buona fede, ritenendo che tale azione sia necessaria per: </p>
+    <Ul>
+            <li> Per ottemperare a un obbligo legale </li>
+            <li> Per proteggere e difendere i diritti o la proprietà di Telefonia Bellini </li>
+            <li> Per prevenire o indagare su possibili illeciti in relazione al servizio </li>
+            <li> Per proteggere la sicurezza personale degli utenti del servizio o del pubblico </li>
+            <li> Per proteggere contro la responsabilità legale </li>
+
+        2487/5000
     </ul>
 
-    <h4>Usage Data</h4>
+    <h2> Sicurezza dei dati </h2>
+    <p> La sicurezza dei tuoi dati è importante per noi, ma ricorda che nessun metodo di trasmissione su Internet o metodo di archiviazione elettronica è sicuro al 100%. Mentre ci sforziamo di utilizzare mezzi commercialmente accettabili per proteggere i tuoi dati personali, non possiamo garantire la sua assoluta sicurezza. </p>
 
-    <p>We may also collect information how the Service is accessed and used ("Usage Data"). This Usage Data may include information such as your computer's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</p>
+    <h2> Fornitori di servizi </h2>
+    <p> Potremmo impiegare società e individui di terze parti per facilitare il nostro servizio ("Fornitori di servizi"), fornire il sito per nostro conto, eseguire servizi relativi ai servizi o aiutarci ad analizzare come viene utilizzato il sito web. </p>
+    <p> Queste terze parti hanno accesso ai tuoi dati personali solo per eseguire queste attività per nostro conto e sono obbligate a non rivelarle o utilizzarle per altri scopi. </p>
 
-    <h4>Tracking & Cookies Data</h4>
-    <p>We use cookies and similar tracking technologies to track the activity on our Service and hold certain information.</p>
-    <p>Cookies are files with small amount of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Tracking technologies also used are beacons, tags, and scripts to collect and track information and to improve and analyze our Service.</p>
-    <p>You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.</p>
-    <p>Examples of Cookies we use:</p>
+
+
+    <h2> Collegamenti ad altri siti </h2>
+    <p> Il nostro servizio potrebbe contenere collegamenti ad altri siti che non sono gestiti da noi. Se fai clic su un link di terze parti, sarai indirizzato al sito di quella terza parte. Ti consigliamo vivamente di rivedere l'Informativa sulla privacy di ogni sito che visiti. </p>
+    <p> Non abbiamo alcun controllo e non ci assumiamo alcuna responsabilità per il contenuto, le politiche sulla privacy o le pratiche di qualsiasi sito o servizio di terze parti. </p>
+
+
+    <h2> Privacy dei minori </h2>
+    <p> Il nostro servizio non si rivolge a minori di 18 anni. </p>
+    <p> Non raccogliamo consapevolmente informazioni di identificazione personale da chiunque abbia meno di 18 anni. Se sei un genitore o un tutore e sei a conoscenza del fatto che i tuoi figli ci hanno fornito dati personali, ti preghiamo di contattarci. Se veniamo a conoscenza del fatto che abbiamo raccolto dati personali da minori senza verifica del consenso dei genitori, adottiamo provvedimenti per rimuovere tali informazioni dai nostri server. </p>
+
+
+    <h2> Modifiche alla presente informativa sulla privacy </h2>
+    <p> Potremmo aggiornare la nostra politica sulla privacy di volta in volta. Ti informeremo di eventuali modifiche pubblicando la nuova Informativa sulla privacy in questa pagina. </P>
+    <p> Ti faremo sapere via e-mail e / o una nota importante sul nostro servizio, prima che la modifica diventi effettiva e aggiorni la "data effettiva" nella parte superiore di questa Informativa sulla privacy. </p>
+    <p> Si consiglia di rivedere periodicamente questa Informativa sulla privacy per eventuali modifiche. Le modifiche a questa Informativa sulla privacy sono efficaci quando sono pubblicate su questa pagina. </p>
+
+
+    <h2> Contattaci </h2>
+    <p> Se hai domande sulla presente Informativa sulla privacy, ti preghiamo di contattarci: </p>
     <ul>
-        <li><strong>Session Cookies.</strong> We use Session Cookies to operate our Service.</li>
-        <li><strong>Preference Cookies.</strong> We use Preference Cookies to remember your preferences and various settings.</li>
-        <li><strong>Security Cookies.</strong> We use Security Cookies for security purposes.</li>
+        <li> Per email: telefoniabellini@outlook.it </li>
+        <li> Visitando questa pagina sul nostro sito Web: https://telefoniabellini.it/ </li>
+
     </ul>
 
-    <h2>Use of Data</h2>
-
-    <p>Telefonia Belliini uses the collected data for various purposes:</p>
-    <ul>
-        <li>To provide and maintain the Service</li>
-        <li>To notify you about changes to our Service</li>
-        <li>To allow you to participate in interactive features of our Service when you choose to do so</li>
-        <li>To provide customer care and support</li>
-        <li>To provide analysis or valuable information so that we can improve the Service</li>
-        <li>To monitor the usage of the Service</li>
-        <li>To detect, prevent and address technical issues</li>
-    </ul>
-
-    <h2>Transfer Of Data</h2>
-    <p>Your information, including Personal Data, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.</p>
-    <p>If you are located outside Italy and choose to provide information to us, please note that we transfer the data, including Personal Data, to Italy and process it there.</p>
-    <p>Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.</p>
-    <p>Telefonia Belliini will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.</p>
-
-    <h2>Disclosure Of Data</h2>
-
-    <h3>Legal Requirements</h3>
-    <p>Telefonia Belliini may disclose your Personal Data in the good faith belief that such action is necessary to:</p>
-    <ul>
-        <li>To comply with a legal obligation</li>
-        <li>To protect and defend the rights or property of Telefonia Belliini</li>
-        <li>To prevent or investigate possible wrongdoing in connection with the Service</li>
-        <li>To protect the personal safety of users of the Service or the public</li>
-        <li>To protect against legal liability</li>
-    </ul>
-
-    <h2>Security Of Data</h2>
-    <p>The security of your data is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Data, we cannot guarantee its absolute security.</p>
-
-    <h2>Service Providers</h2>
-    <p>We may employ third party companies and individuals to facilitate our Service ("Service Providers"), to provide the Service on our behalf, to perform Service-related services or to assist us in analyzing how our Service is used.</p>
-    <p>These third parties have access to your Personal Data only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose.</p>
-
-
-
-    <h2>Links To Other Sites</h2>
-    <p>Our Service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.</p>
-    <p>We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
-
-
-    <h2>Children's Privacy</h2>
-    <p>Our Service does not address anyone under the age of 18 ("Children").</p>
-    <p>We do not knowingly collect personally identifiable information from anyone under the age of 18. If you are a parent or guardian and you are aware that your Children has provided us with Personal Data, please contact us. If we become aware that we have collected Personal Data from children without verification of parental consent, we take steps to remove that information from our servers.</p>
-
-
-    <h2>Changes To This Privacy Policy</h2>
-    <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
-    <p>We will let you know via email and/or a prominent notice on our Service, prior to the change becoming effective and update the "effective date" at the top of this Privacy Policy.</p>
-    <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
-
-
-    <h2>Contact Us</h2>
-    <p>If you have any questions about this Privacy Policy, please contact us:</p>
-    <ul>
-        <li>By email: telefoniabellini@outlook.it </li>
-        <li>By visiting this page on our website: https://telefoniabellini.it/</li>
-
-    </ul>
 
 </div>
 
