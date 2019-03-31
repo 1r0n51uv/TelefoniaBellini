03f14aae2dd3d003a2ec6444a2c5a404ef5fb8e9
ironsiuv <s.ammirati95@gmail.com>
2019-03-31 17:24:40 +0200

    - Updated home with promotional phone companies offer

--- resources/views/components/indexNews.blade.php
+++ resources/views/components/indexNews.blade.php
@@ -12,20 +12,45 @@
                         <h4><i class="fas fa-mobile"></i> 339xxxxxx</h4>
                     </div>
 
-
                     <div class="col-md-4" style="text-align: center; margin-top: 2%">
 
                         <h2><i class="fas fa-cash-register"></i> Vuoi permutare il tuo usato?</h2>
                         <h5>Fissa un appuntamento e <b>gratuitamente</b> valuteremo approssimativamente il tuo usato</h5>
 
                     </div>
-
-
                     <div class="col-md-4" style="text-align: center; margin-top: 2%">
                         <h2><i class="fas fa-truck-loading"></i> Spedizioni in tutta Italia</h2>
                         <h3>Gratuite su ordini superiori ai <b>200€</b></h3>
 
                     </div>
+                </div>
+
+                <hr>
+                <div class="row">
+
+                    <div class="col-md-5" style="text-align: center; margin-top: 2%">
+
+                        <h2><i class="fas fa-tools"></i> Riparazione smartphone</h2>
+                        <h3>Fissa un appuntamento ed avrai un preventivo <b>gratuito</b> per la riparazione</h3>
+
+                    </div>
+
+                    <div class="col-md-7" style="text-align: center; margin-top: 2%">
+
+                        <h2><i class="fas fa-comments-dollar"></i> Promozioni operatore</h2>
+                        <div class="row justify-content-center">
+                            <div class="col-md-12">
+                                <img src="../assets/image/home/tim-min.png" width="45%" class="img-responsive" alt="">
+                            </div>
+
+                            <div class="col-md-12" style="margin-top: 5%">
+                                <img src="../assets/image/home/vodafone-min.png" width="60%" class="img-responsive" alt="">
+                            </div>
+                            
+                        </div>
+
+                    </div>
+
                 </div>
 
             </div>
